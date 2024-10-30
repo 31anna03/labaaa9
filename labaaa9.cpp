69729c6b554dc47e5e12ba57494ed9918e9403a5
annak <annak@DESKTOP-MGDTLFC>
2024-10-30 22:15:52 +0200

    Добавьте файлы проекта.

---
+++ labaaa9.cpp
@@ -0,0 +1,33 @@
+﻿// Створити чергу цілих чисел. Обчислити суму парних елементів черги.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    queue<int> q; 
+    int n, x, sum = 0;
+    cout << "Введіть розмір черги: "; 
+    cin >> n;
+    cout << "Введіть " << n << " цілих чисел: \n";
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        q.push(x);
+    }
+    while (!q.empty()) {
+        if (q.front() % 2 == 0) {
+            sum += q.front();
+        }
+        q.pop();
+    }
+
+    cout << "Сума парних елементів у черзі: " << sum << endl;
+    return 0;
+}
+
